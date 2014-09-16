10f3a4e4b51df4fa0a91fe2cb3d2a90386e5bbb8
pekeCS147 <rerich@stanford.edu>
2014-09-16 13:34:48 -0700

    Adding in more details: location, profile photo, likes count and heart logo and some formatting.

---
+++ InstagramViewerPreTwoComments/app/src/main/java/com/example/rrich/instagramviewer/BitmapScaler.java
@@ -0,0 +1,24 @@
+package com.example.rrich.instagramviewer;
+
+import android.graphics.Bitmap;
+
+/**
+ * Created by rrich on 9/14/14.
+ */
+public class BitmapScaler {
+    // scale and keep aspect ratio
+    public static Bitmap scaleToFitWidth(Bitmap b, int width)
+    {
+        float factor = width / (float) b.getWidth();
+        return Bitmap.createScaledBitmap(b, width, (int) (b.getHeight() * factor), true);
+    }
+
+
+    // scale and keep aspect ratio
+    public static Bitmap scaleToFitHeight(Bitmap b, int height)
+    {
+        float factor = height / (float) b.getHeight();
+        return Bitmap.createScaledBitmap(b, (int) (b.getWidth() * factor), height, true);
+    }
+
+}
---
+++ InstagramViewerPreTwoComments/app/src/main/java/com/example/rrich/instagramviewer/InstagramPhoto.java
@@ -0,0 +1,21 @@
+package com.example.rrich.instagramviewer;
+
+/**
+ * Created by rrich on 9/13/14.
+ */
+public class InstagramPhoto {
+    public String username;
+    public String caption;
+    public String imageUrl;
+    public String profileImageUrl;
+    public int imageHeight;
+    public int likesCount;
+    public String locationName;
+    public long createdTime;
+
+    public String toString() {
+        return "hello - " + imageUrl;
+    }
+
+
+}
